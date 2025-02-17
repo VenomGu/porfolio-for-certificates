b8156a98c01f39d34bd4247c34980db53a56f36f
Gustavo Calastro <gu.calastro@gmail.com>
2025-02-17 19:24:38 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 document.getElementById('check-btn').addEventListener('click', function() {
-
     const inputText = document.getElementById('text-input').value.trim();
     
     function isPalindrome(str) {
         const cleanStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
         const reversedStr = cleanStr.split('').reverse().join('');
-        
         return cleanStr === reversedStr;
     }
 
     const resultDiv = document.getElementById('result');
 
-    inputText == "" ? resultDiv.innerHTML = alert('Please input a value.') : isPalindrome(inputText) ? resultDiv.innerHTML = `"${inputText}" is a palindrome!` : resultDiv.innerHTML = `"${inputText}" is not a palindrome.`;
+    if (inputText === "") {
+        alert('Please input a value.');
+    } else {
+        if (isPalindrome(inputText)) {
+            resultDiv.innerHTML = `${inputText} is a palindrome!`;  
+        } else {
+            resultDiv.innerHTML = `${inputText} is not a palindrome.`;  
+        }
+    }
 });
